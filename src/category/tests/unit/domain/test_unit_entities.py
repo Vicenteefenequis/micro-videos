6561c6679b3e2b3b9824f85e0743ba27eb3fd28a
Vicente Junior <vicente19981@live.com>
2023-05-28 14:30:34 -0300

    test: ensure Category is data class

--- src/category/tests/unit/domain/test_unit_entities.py
+++ src/category/tests/unit/domain/test_unit_entities.py
@@ -1,12 +1,21 @@
 import unittest
 from datetime import datetime
+from dataclasses import is_dataclass
 from category.domain.entities import Category
 
 
 class TestCategory(unittest.TestCase):
 
+    def test_if_is_a_dataclass(self):
+        self.assertTrue(is_dataclass(Category))
+
     def test_constructor(self):
-        category = Category('Movie','some description',True, datetime.now())
+        category = Category(
+            name='Movie',
+            description='some description',
+            is_active=True,
+            created_at=datetime.now()
+        )
         self.assertEqual(category.name, 'Movie')
         self.assertEqual(category.description, 'some description')
         self.assertEqual(category.is_active, True)
